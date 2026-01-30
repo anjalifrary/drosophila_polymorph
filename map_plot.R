95b971940a31d0a06075b76bab7edf140fdc0850
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-01-30 11:03:15 -0500

    update

---
+++ map_plot.R
@@ -0,0 +1,33 @@
+# Libraries
+library(data.table)
+library(maps)
+library(ggplot2)
+
+
+##### A) Map
+#### samples file
+samps <- fread("/project/berglandlab/anjali/drosophila_polymorphism/metadata/simulans_pooled.meta.use.csv")
+samps[grepl(":", oldName),set:="Prev. Published"]
+samps[!grepl(":", oldName),set:="Contaminated DEST"]
+
+world <- map_data("world")
+
+a <- ggplot() +
+  geom_map(
+    data = world, map = world,
+    aes(x = long, y = lat, map_id = region),
+    fill = "#cccccc", color = "darkgray", linewidth = 0.1
+  ) +
+  theme_classic(base_size = 14) +
+  coord_sf() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_blank(), axis.text = element_text(color = "black")) +
+  scale_fill_manual(values = c("#f27f65","#8c89c1","#a5c9cc")) +
+  scale_color_manual(values = c("#D05438","#615E93","#6F9FA3")) +
+  theme(legend.position = "bottom") +
+  geom_point(
+    data = samps,
+    aes(x=long,
+        y=lat,
+        fill = set, color = set), size = 2.5, shape = 21
+  )
+a
