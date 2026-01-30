f5bc006fad4efa6f6232c0a4d662a618c57fea32
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-01-30 11:10:25 -0500

    debug

--- map_plot.R
+++ map_plot.R
@@ -13,21 +13,30 @@ samps[!grepl(":", oldName),set:="Contaminated DEST"]
 world <- map_data("world")
 
 a <- ggplot() +
-  geom_map(
-    data = world, map = world,
+  geom_polygon(
+    data = world,
     aes(x = long, y = lat, map_id = region),
-    fill = "#cccccc", color = "darkgray", linewidth = 0.1
-  ) +
-  theme_classic(base_size = 14) +
-  coord_sf() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_blank(), axis.text = element_text(color = "black")) +
-  scale_fill_manual(values = c("#f27f65","#8c89c1","#a5c9cc")) +
-  scale_color_manual(values = c("#D05438","#615E93","#6F9FA3")) +
-  theme(legend.position = "bottom") +
+    fill = "#cccccc", 
+    color = "darkgray", 
+    linewidth = 0.1
+  ) + 
   geom_point(
     data = samps,
     aes(x=long,
         y=lat,
-        fill = set, color = set), size = 2.5, shape = 21
-  )
+        fill = set, color = set), 
+        size = 2.5, 
+        shape = 21
+  ) +
+  theme_classic(base_size = 14) +
+  coord_quickmap() +
+  scale_fill_manual(values = c("#f27f65","#8c89c1","#a5c9cc")) +
+  scale_color_manual(values = c("#D05438","#615E93","#6F9FA3")) +
+  theme(legend.position = "bottom",
+  panel.grid.major = element_blank(), 
+  panel.grid.minor = element_blank(), 
+  panel.background = element_blank(), 
+  axis.line = element_blank(), 
+  axis.text = element_text(color = "black")
+  ) 
 a
