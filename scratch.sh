5ad05bee87596cd404e6b8e894266fb4de372151
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-01-30 10:58:50 -0500

    added map

---
+++ scratch.sh
@@ -0,0 +1,5 @@
+#ijob -A berglandlab -c2 -p standard --mem=40G
+#export R_LIBS_USER="/sfs/gpfs/tardis/home/ejy4bu/R/goolf/4.5/"
+#module load gcc/11.4.0  openmpi/4.1.4 icu R/4.5.0
+#module load R/4.5.0
+#R
