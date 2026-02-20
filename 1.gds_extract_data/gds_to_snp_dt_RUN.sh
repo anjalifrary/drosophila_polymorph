f2ae541687be8ad60f06e3acbadf4781d656b39c
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-02-20 11:25:26 -0500

    .

--- 1.gds_extract_data/gds_to_snp_dt_RUN.sh
+++ 1.gds_extract_data/gds_to_snp_dt_RUN.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1         # Single task per job
 #SBATCH --cpus-per-task=10 # Number of CPU cores per task
 #SBATCH -N 1               # Run on one node
-#SBATCH -t 0-1:00          # 1 hours runtime
+#SBATCH -t 0-10:00         # 10 hours runtime
 #SBATCH --mem=100G         # Memory per node
 #SBATCH -o /scratch/ejy4bu/err_outs/GDS/gdsDT.%A_%a.out  # Standard output
 #SBATCH -e /scratch/ejy4bu/err_outs/GDS/gdsDT.%A_%a.err  # Standard error
