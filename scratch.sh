f7ec44ae13019145c5bc3cf7f350654637babac3
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-02-19 23:14:20 -0500

    .

--- scratch.sh
+++ scratch.sh
@@ -6,14 +6,14 @@ R
 
 
 /// 
-# !/usr/bin/env bash
-# SBATCH -J jobname         # Job name
-# SBATCH --ntasks=1         # Single task per job
-# SBATCH --cpus-per-task=10 # Number of CPU cores per task
-# SBATCH -N 1               # Run on one node
-# SBATCH -t 0-10:00         # 10 hours runtime
-# SBATCH --mem=100G         # Memory per node
-# SBATCH -o /scratch/ejy4bu/err_outs/name.%A_%a.out  # Standard output
-# SBATCH -e /scratch/ejy4bu/err_outs/name.%A_%a.err  # Standard error
-# SBATCH -p standard       # Partition
-# SBATCH --account=berglandlab
+#!/usr/bin/env bash
+#SBATCH -J jobname         # Job name
+#SBATCH --ntasks=1         # Single task per job
+#SBATCH --cpus-per-task=10 # Number of CPU cores per task
+#SBATCH -N 1               # Run on one node
+#SBATCH -t 0-10:00         # 10 hours runtime
+#SBATCH --mem=100G         # Memory per node
+#SBATCH -o /scratch/ejy4bu/err_outs/name.%A_%a.out  # Standard output
+#SBATCH -e /scratch/ejy4bu/err_outs/name.%A_%a.err  # Standard error
+#SBATCH -p standard       # Partition
+#SBATCH --account=berglandlab
