2d3a723268a2dc74bff971e657deb1d8b4503624
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-02-19 23:12:49 -0500

    making run script

---
+++ 1.gds_extract_data/gds_to_snp_dt_RUN.sh
@@ -0,0 +1,13 @@
+# !/usr/bin/env bash
+# SBATCH -J gdsToDT         # Job name
+# SBATCH --ntasks=1         # Single task per job
+# SBATCH --cpus-per-task=10 # Number of CPU cores per task
+# SBATCH -N 1               # Run on one node
+# SBATCH -t 0-10:00         # 10 hours runtime
+# SBATCH --mem=100G         # Memory per node
+# SBATCH -o /scratch/ejy4bu/err_outs/gdsDT.%A_%a.out  # Standard output
+# SBATCH -e /scratch/ejy4bu/err_outs/gdsDT.%A_%a.err  # Standard error
+# SBATCH -p standard       # Partition
+# SBATCH --account=berglandlab
+
+Rscript gds_to_snp_dt.R
--- scratch.sh
+++ scratch.sh
@@ -3,3 +3,17 @@ export R_LIBS_USER="/sfs/gpfs/tardis/home/ejy4bu/R/goolf/4.5/"
 module load gcc/11.4.0  openmpi/4.1.4 icu R/4.5.0
 module load R/4.5.0
 R
+
+
+/// 
+# !/usr/bin/env bash
+# SBATCH -J jobname         # Job name
+# SBATCH --ntasks=1         # Single task per job
+# SBATCH --cpus-per-task=10 # Number of CPU cores per task
+# SBATCH -N 1               # Run on one node
+# SBATCH -t 0-10:00         # 10 hours runtime
+# SBATCH --mem=100G         # Memory per node
+# SBATCH -o /scratch/ejy4bu/err_outs/name.%A_%a.out  # Standard output
+# SBATCH -e /scratch/ejy4bu/err_outs/name.%A_%a.err  # Standard error
+# SBATCH -p standard       # Partition
+# SBATCH --account=berglandlab
