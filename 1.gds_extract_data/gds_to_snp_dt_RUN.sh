7b2ec50d43fbd1308a8c468587f78bed3887fc34
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-02-20 10:58:34 -0500

    debug

--- 1.gds_extract_data/gds_to_snp_dt_RUN.sh
+++ 1.gds_extract_data/gds_to_snp_dt_RUN.sh
@@ -3,12 +3,14 @@
 #SBATCH --ntasks=1         # Single task per job
 #SBATCH --cpus-per-task=10 # Number of CPU cores per task
 #SBATCH -N 1               # Run on one node
-#SBATCH -t 0-10:00         # 10 hours runtime
+#SBATCH -t 0-1:00          # 1 hours runtime
 #SBATCH --mem=100G         # Memory per node
 #SBATCH -o /scratch/ejy4bu/err_outs/GDS/gdsDT.%A_%a.out  # Standard output
 #SBATCH -e /scratch/ejy4bu/err_outs/GDS/gdsDT.%A_%a.err  # Standard error
-#SBATCH -p standard       # Partition
+#SBATCH -p standard        # Partition
 #SBATCH --account=berglandlab
 
 cd 1./gds_extract_data
+export R_LIBS_USER="/sfs/gpfs/tardis/home/ejy4bu/R/goolf/4.5/"
+module load gcc/11.4.0  openmpi/4.1.4 icu R/4.5.0
 Rscript gds_to_snp_dt.R
