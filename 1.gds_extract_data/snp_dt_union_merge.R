2d166d0e2a120a19406a0148dcb29e14f16cf68f
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-03-17 21:16:35 -0400

    shared merge complet

--- 1.gds_extract_data/snp_dt_union_merge.R
+++ 1.gds_extract_data/snp_dt_union_merge.R
@@ -12,13 +12,11 @@ if(!file.exists(out_rds)) file.create(out_rds)
 mel_snp_rds <- paste0(out_dir, "mel_snp_dt.rds")
 mel_snp_dt <- readRDS(mel_snp_rds)
 sim_snp_rds <- paste0(out_dir, "sim_snp_dt.rds")
-sim_snp_dt <- readRDS(sim_snp_dt)
+sim_snp_dt <- readRDS(sim_snp_rds)
 
-shared <- merge(mel_snp_dt, sim_snp_dt, by = c("chr", "pos", "ref", "alt"), suffixes = c("_mel", "_sim"), all=T)
-message(nrow(shared), " total variants")
+shared_table <- merge(mel_snp_dt, sim_snp_dt, by = c("chr", "pos", "ref", "alt"), suffixes = c("_mel", "_sim"), all=T)
+message(nrow(shared_table), " total variants")
 
-# shared_test <- shared[1:100]
-# shared_table <- get_gds_data(mel_gds, shared, "mel")
 message("saving csv to ", out_csv)
 fwrite(shared_table, out_csv)
 message("saving rds to ", out_rds)
