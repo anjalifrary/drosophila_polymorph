ea121f77a664058489a8a2b421f54d759927c14f
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-03-18 13:40:14 -0400

    remake complete merged dt

--- 1.gds_extract_data/snp_dt_merge.R
+++ 1.gds_extract_data/snp_dt_merge.R
@@ -4,8 +4,8 @@ library(foreach)
 library(doMC)
 
 out_dir <- "/scratch/ejy4bu/drosophila/gds_analysis/snp_datatables/"
-out_csv <- paste0(out_dir, "test_files/all_variants_chr2L_500test.csv")
-out_rds <- paste0(out_dir, "test_files/all_variants_chr2L.rds")
+out_csv <- paste0(out_dir, "all_variants_500test.csv")
+out_rds <- paste0(out_dir, "all_variants_chr2L.rds")
 if(!file.exists(out_csv)) file.create(out_csv)
 if(!file.exists(out_rds)) file.create(out_rds)
 
@@ -14,12 +14,12 @@ mel_snp_dt <- readRDS(mel_snp_rds)
 sim_snp_rds <- paste0(out_dir, "sim_snp_dt.rds")
 sim_snp_dt <- readRDS(sim_snp_rds)
 
-### test on chromosome 2L
-# filter by 2L chromosome for a smaller subset to test merge on for csv readable
-mel_snp_dt <- mel_snp_dt[chr == "2L"]
-message(nrow(mel_snp_dt), " mel 2L variants")
-sim_snp_dt <- sim_snp_dt[chr == "2L"]
-message(nrow(sim_snp_dt), " sim 2L variants")
+# ### test on chromosome 2L
+# # filter by 2L chromosome for a smaller subset to test merge on for csv readable
+# mel_snp_dt <- mel_snp_dt[chr == "2L"]
+# message(nrow(mel_snp_dt), " mel 2L variants")
+# sim_snp_dt <- sim_snp_dt[chr == "2L"]
+# message(nrow(sim_snp_dt), " sim 2L variants")
 
 # NOTE: this is a union merge... keep ALL variants. no filtering yet
 shared_table <- merge(mel_snp_dt, sim_snp_dt, by = c("chr", "pos"), suffixes = c("_mel", "_sim"), all=T)
