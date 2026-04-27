04b77d9ff14ca6086afdf41e2ec1eb58ede98ce1
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-04-27 18:22:56 -0400

    remade prelim classification scheme with most up to date filtered / quality tabel

--- 2.classification_prelim/classification_diffPosition_sameCodon.R
+++ 2.classification_prelim/classification_diffPosition_sameCodon.R
@@ -1,6 +1,62 @@
+################################################################################################
+# for the classification of different site but same codon, same / different nucleotides, same / different amino acids 
+################################################################################################
+
 library(data.table)
 
-rds_file <- paste0("/scratch/ejy4bu/drosophila/gds_analysis/snp_dt_analysis/all_variants_clean.rds")
+rds_file <- paste0("/scratch/ejy4bu/drosophila/gds_analysis/snp_dt_analysis/currentFiles/subset_variantsOfInterest_qualFiltered.rds")
 shared_dt <- readRDS(rds_file)
 message(nrow(shared_dt), " total variants in shared table")
 
+
+shared_dt[
+    !is.na(ref_mel) & # start with mel rows, don't need to do sim bc all should reference each other but checking... 
+    (
+        adjacent_var_pos1 %in% shared_dt[!is.na(ref_sim), pos] |
+        adjacent_var_pos2 %in% shared_dt[!is.na(ref_sim), pos] |
+        adjacent_var_pos3 %in% shared_dt[!is.na(ref_sim), pos]
+    ),
+    classification := ifelse(
+    is.na(classification),
+    "I",
+    paste0(classification, ";I")
+    )
+]
+table(shared_dt$classification)
+#  A   A;I     B   B;I     C   C;I     D   D;I     E   E;I     F   F;I     G
+# 58653 21971   215   138 22483  7529 14424  5705    52    18  2903  1512  2568
+#   G;I     H   H;I     I
+#  1371   177    73 58620
+
+#      A  A;I;I      B  B;I;I      C  C;I;I      D  D;I;I      E  E;I;I      F
+#  58653  21971    215    138  22483   7529  14424   5705     52     18   2903
+#  F;I;I      G  G;I;I      H  H;I;I      I
+#   1512   2568   1371    177     73 187142
+shared_dt[
+    !is.na(ref_sim) & # checking sim, should be same
+    (
+        adjacent_var_pos1 %in% shared_dt[!is.na(ref_mel), pos] |
+        adjacent_var_pos2 %in% shared_dt[!is.na(ref_mel), pos] |
+        adjacent_var_pos3 %in% shared_dt[!is.na(ref_mel), pos]
+    ),
+    classification := ifelse(
+    is.na(classification),
+    "I",
+    paste0(classification, ";I")
+    )
+]
+
+table(shared_dt$classification) # should be same as before
+
+# clean up
+shared_dt[
+    ,
+    classification := gsub("(^I$)|(;I$)", "", classification)
+]
+table(shared_dt$classification) # should be same as before
+
+unique_codons_I <- unique(
+    shared_dt[grepl("(^|;)I($|;)", classification),
+              .(gene_id_mel, codon_start_pos)]
+)
+message(length(unique_codons_I))
--- 2.classification_prelim/classification_samePosition.R
+++ 2.classification_prelim/classification_samePosition.R
@@ -1,10 +1,10 @@
 ################################################################################################
-# for the classification of same site, same nt, same AND different amino acids 
+# for the classification of same site, same / different nucleotides, same / different amino acids 
 ################################################################################################
 
 library(data.table)
 
-rds_file <- paste0("/scratch/ejy4bu/drosophila/gds_analysis/snp_dt_analysis/all_variants_clean.rds")
+rds_file <- paste0("/scratch/ejy4bu/drosophila/gds_analysis/snp_dt_analysis/currentFiles/subset_variantsOfInterest_qualFiltered.rds")
 shared_dt <- readRDS(rds_file)
 message(nrow(shared_dt), " total variants in shared table")
 
@@ -21,31 +21,33 @@ shared_dt[
 ]
 shared_classA <- shared_dt[classification == "A"]
 message(nrow(shared_classA), " shared variants with same pos, nt, and aa")
-class_dt[class_dt$Classification == "A", "Count"] <- nrow(shared_classA)
+class_dt$Count[class_dt$Classification == "A"] <- nrow(shared_classA)
 
 ### CLASS B: same position, same nt, diff amino acid ###########################################
 shared_dt[
     ref_mel == ref_sim &
     alt_mel == alt_sim & 
-    aa_ref_mel == aa_ref_sim & 
-    aa_alt_mel != aa_alt_sim,
-    classification := "B"
+    aa_ref_mel != aa_ref_sim & 
+    aa_alt_mel == aa_alt_sim,
+    classification := "B2"
 ]
-shared_classB <- shared_dt[classification == "B"]
+shared_classB <- shared_dt[classification == "B2"]
 message(nrow(shared_classB), " shared variants with same pos, nt, and diff aa")
-class_dt[class_dt$Classification == "B", "Count"] <- nrow(shared_classB)
+class_dt$Count[class_dt$Classification == "B2"] <- nrow(shared_classB)
 
+shared_classB <- NULL
 ### CLASS C: same position, diff nt, same amino acid ############################################
 shared_dt[
     ref_mel == ref_sim &
     alt_mel != alt_sim & 
     aa_ref_mel == aa_ref_sim & 
     aa_alt_mel == aa_alt_sim,
-    classification := "C"
+    classification := "C1"
 ]
-shared_classC <- shared_dt[classification == "C"]
+# shared_classC <- NULL
+shared_classC <- shared_dt[classification == "C1"]
 message(nrow(shared_classC), " shared variants with same pos, diff nt, and same aa")
-class_dt[class_dt$Classification == "C", "Count"] <- nrow(shared_classC)
+class_dt$Count[class_dt$Classification == "C1"] <- nrow(shared_classC)
 
 ### CLASS D: same position, diff nt, diff amino acid ###########################################
 shared_dt[
@@ -53,12 +55,26 @@ shared_dt[
     alt_mel != alt_sim & 
     aa_ref_mel == aa_ref_sim & 
     aa_alt_mel != aa_alt_sim,
-    classification := "D"
+    classification := "D1"
 ]
-shared_classD <- shared_dt[classification == "D"]
+shared_classD <- shared_dt[classification == "D1"]
 message(nrow(shared_classD), " shared variants with same pos, diff nt, and diff aa")
-class_dt[class_dt$Classification == "D", "Count"] <- nrow(shared_classD)
+class_dt$Count[class_dt$Classification == "D1"] <- nrow(shared_classD)
+shared_classD <- NULL
 
+
+## CLASS X: testing new combos at same position ############################################
+shared_dt[
+    ref_mel == ref_sim &
+    alt_mel == alt_sim & 
+    aa_ref_mel != aa_ref_sim & 
+    aa_alt_mel != aa_alt_sim,
+    classification := "E"
+]
+shared_classX <- shared_dt[classification == "E"]
+message(nrow(shared_classX), " shared variants in class X")
+class_dt$Count[class_dt$Classification=="E"] <- nrow(shared_classX)
+shared_classX <- NULL
 
 ############ UPDATE FILES ###############################
 # outputs summary of classification stats 
@@ -70,24 +86,9 @@ saveRDS(shared_dt, rds_file)
 # CSV SUBSET
 subset_table <- shared_dt[1:500, ]
 table(subset_table$classification, useNA = "ifany")
-fwrite(subset_table, "/scratch/ejy4bu/drosophila/gds_analysis/snp_dt_analysis/all_variants_clean_500test.csv")
+fwrite(subset_table, "/scratch/ejy4bu/drosophila/gds_analysis/snp_dt_analysis/subset_variantsOfInterest_qualFiltered_500test.csv")
 # CSV CLASSICATION
 fwrite(class_dt, csv_class)
 
 #########################################################
 
-
-# ## CLASS X: testing new combos at same position ############################################
-# shared_dt[
-#     ref_mel == ref_sim &
-#     alt_mel == alt_sim & 
-#     aa_ref_mel == aa_ref_sim & 
-#     aa_alt_mel != aa_alt_sim,
-#     classification := "B"
-# ]
-# shared_dt[classification == "X", classification := NA]
-
-# # shared_classX <- shared_dt[classification == "X"] # clear classification with class X
-# message(nrow(shared_dt[classification == "X"]), " shared variants in class X")
-
-# class_dt[class_dt$Classification=="H", "Count" ] <- nrow(shared_dt[classification == "H"])
