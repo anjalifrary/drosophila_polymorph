c46d6d314070c677faf02c2a2fcefa9b1fb99867
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-07-20 17:37:55 -0400

    .

---
+++ 5.GO/gowinda_analysis_new07-2026.R
@@ -0,0 +1,187 @@
+library(data.table)
+library(dplyr)
+
+
+# column 1: the GO term
+# column 2: on the average this number of genes are found per simulation for the given GO category. In --mode gene every gene is only counted once whereas in --mode snp a single gene may be counted several times dependent on the SNP
+# column 3: using the candidate SNPs this number of genes was found for the given GO category. In --mode gene every gene is only counted once whereas in --mode snp a single gene may be counted several times dependent on the SNP
+# column 4: p-value (uncorrected for multiple testing)
+# column 5: FDR (p-value after adjustment for multiple testing)
+# column 6: the number of genes (uniq) found for the given GO category
+# column 7: the number of genes that could at most be found for the given GO category, i.e.: genes of the given GO category that have an corresponding entry in the annotation file and contain at least one SNP
+# column 8: total number of genes for the given GO category in the GO association file
+# column 9: description of the given GO term
+# column 10: comma separated list of the gene_ids found for the given GO category
+
+cols <- c("GO.id", "SimulatedGenes", "ObservedGenes", 
+  "p.value", "FDR", 
+  "N_GenesFound", "N_GenesMax", "N_GenesTotal",
+  "Description", "Genes")
+
+
+
+library(ontologyIndex)
+go_file <- get_ontology("/scratch/ejy4bu/drosophila/gowinda/go.obo")
+
+#####################################################
+# add ontology 
+library(AnnotationDbi)
+library(GO.db)
+go_dt <- data.table(GO.id = keys(GO.db))
+go_dt[, ontology := Ontology(GO.id)]
+# gowinda_results from line 22, modify for each results file
+# gowinda_results <- merge(gowinda_results, go_dt, by="GO.id", all.x=T)
+
+
+parse_gowinda_metadata <- function(file){
+    fname <- basename(file)
+    dir_parts <- strsplit(dirname(file), "/")[[1]]
+
+    # filename
+    # gowinda_XY_1_polyAF.txt
+    fname_parts <- strsplit(sub("\\.txt$", "", fname), "_")[[1]]
+
+    classes <- fname_parts[2]
+    maf_value <- fname_parts[3]
+    maf_def <- fname_parts[4]
+
+    # directory pieces
+    maf_dir <- dir_parts[grepl("MAF", dir_parts)]
+    bg_dir <- dir_parts[grepl("^bg_", dir_parts)]
+
+    background <- sub("^bg_", "", bg_dir)
+
+    data.table(
+        classes = classes,
+        MAF_value = as.numeric(maf_value),
+        MAF_def = maf_def,
+        background = background
+    )
+}
+
+append_gowinda_summary <- function(results, file, csv_file, filter_col="FDR", threshold=0.05){
+    meta <- parse_gowinda_metadata(file)
+    sig <- unique(results[get(filter_col) < threshold, GO.id])
+    new_row <- cbind(
+        meta,
+        data.table(
+            threshold = paste0(filter_col,"<",threshold),
+            N_GOTerms = length(sig),
+            GO.ids = if(length(sig)) paste(sig, collapse=";") else NA_character_
+        )
+    )
+
+    if(file.exists(csv_file)){
+        out <- rbind(fread(csv_file), new_row, fill=TRUE)
+    } else {
+        out <- new_row
+        file.create(csv_file)
+    }
+
+    fwrite(out, csv_file)
+}
+
+
+# csv headers
+# MAF_value | MAF_def | background | classes | statThreshold | N_GOTerms | GO.ids
+out_csv <- "/scratch/ejy4bu/drosophila/gowinda/gowindaRunsStats/gowindaStats_FDR0.05.csv"
+dir.create("/scratch/ejy4bu/drosophila/gowinda/gowindaRunsStats/")
+
+### to loop over a single results directory:
+
+dir <- "/scratch/ejy4bu/drosophila/gowinda/results/MAF0.5filter_polyAF/"
+files_list <- list.files(path = dir, pattern="gowinda_.*txt", recursive = TRUE, full.names = TRUE)
+
+for (file_name in files_list) {
+    # skip empty files
+    if(file.info(file_name)$size == 0){
+        message("Skipping empty file: ", file)
+        next
+    }
+    results <- fread(file_name, header=FALSE, col.names=cols)
+    append_gowinda_summary(results, file_name, out_csv)
+}
+
+
+### to inspect a single file:
+file <- "/scratch/ejy4bu/drosophila/gowinda/results/MAF1filter_polyAF/bg_speciesSpecific_noMAF/gowinda_XY_1_polyAF.txt"
+results <- read.delim(file, header=FALSE, col.names=cols)
+setDT(results)
+
+results$Description <- go_file$name[results$GO.id]
+results <- merge(results, go_dt, by="GO.id", all.x=T)
+
+nrow(unique(results %>% filter(FDR < 0.05)))
+nrow(unique(results %>% filter(p.value < 0.05)))
+
+append_gowinda_summary(results, file, out_csv)
+
+
+
+### FIGURES
+library(data.table)
+library(ggplot2)
+
+csv <- fread(out_csv)
+
+classes="AB"
+bg="speciesSpecific_noMAF"
+# bg="sharedOnly_noMAF"
+maf_def="polyAF"
+# maf_def="globalAF"
+
+plot_counts <- csv[ 
+    classes==classes & 
+    background==bg & 
+    MAF_def==maf_def,
+    .(classes, MAF_value, MAF_def, background, threshold, N_GOTerms, GO.ids)]
+
+# y = number significant go terms
+# x = MAF 
+ggplot(plot_counts, aes(x=MAF_value, y=N_GOTerms)) +
+    geom_point(size=3) +
+    geom_line() +
+    scale_x_continuous(breaks=unique(plot_counts$MAF_value)) +
+    labs(
+        x="MAF filter (%)",
+        y="Number of significant GO terms"
+    ) +
+    theme_classic()
+
+go_long <- csv[
+    classes == "ABFGOPXY" &
+    background == "speciesSpecific_noMAF" &
+    threshold == "FDR<0.05" &
+    GO.ids != "",
+    .(
+        MAF_value,
+        GO.id = unlist(strsplit(GO.ids, ";"))
+    ),
+    by = .(classes, background, threshold, MAF_value)
+]
+
+go_persistence <- go_long[
+    ,
+    .(
+        n_MAF = uniqueN(MAF_value),
+        MAFs = paste(sort(unique(MAF_value)), collapse=",")
+    ),
+    by=GO.id
+]
+
+go_persistence[order(-n_MAF)]
+
+
+ggplot(
+    go_long,
+    aes(
+        x=factor(MAF_value),
+        y=reorder(GO.id, GO.id)
+    )
+) +
+    geom_tile(fill="black") +
+    labs(
+        x="MAF filter (%)",
+        y="GO term"
+    ) +
+    theme_classic()
--- 5.GO/run_gowinda.sh
+++ 5.GO/run_gowinda.sh
@@ -17,11 +17,11 @@ BG_dir=/scratch/ejy4bu/drosophila/gowinda/backgroundFiles/
 CANDIDATE_root=/scratch/ejy4bu/drosophila/gowinda/candidateFiles/
 OUTPUT_root=/scratch/ejy4bu/drosophila/gowinda/results
 
-CANDIDATE_dir=MAF0.5filter_polyAF
-CANDIDATE_id=0.5_polyAF
+CANDIDATE_dir=MAF5filter_polyAF
+CANDIDATE_id=5_polyAF
 
 #maf_inputs <- c(0.005, 0.01, 0.02, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.40, 0.49)
-# running 0.02,
+# running 0.02, 0.01, 0.005
 
 gtf_file=/project/berglandlab/anjali/drosophila_polymorphism/gene_ontology/gowinda/dmel-all-r6.67.gtf
 go_file=/project/berglandlab/anjali/drosophila_polymorphism/gene_ontology/gowinda/flybase_gaf_go.txt
