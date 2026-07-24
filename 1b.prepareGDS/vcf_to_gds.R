8f28d013261ddc8499a4f303ec1fd4b1e7ac82ea
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-07-24 15:34:54 -0400

    reheadered dmel vcf

--- 1b.prepareGDS/vcf_to_gds.R
+++ 1b.prepareGDS/vcf_to_gds.R
@@ -1,7 +1,7 @@
 library(SeqArray)
 
-vcf_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.ann.eff.vcf.gz"
-gds_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.ann.eff.gds"
+vcf_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.vcf.gz"
+gds_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.gds"
 
 seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = TRUE)
 
--- 1b.prepareGDS/vcf_to_gds_RUN.sh
+++ 1b.prepareGDS/vcf_to_gds_RUN.sh
@@ -14,5 +14,17 @@ export R_LIBS_USER=~/Rlibs
 module load gcc/11.4.0 openmpi/4.1.4 
 module load R/4.5.0
 
+# # reheader dmel file
+# vcf=/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.ann.eff.vcf.gz
+# bcftools view -h $vcf \
+# > /scratch/ejy4bu/drosophila/inbred/combined_vcf/DRGP2.dm6.header.txt
+
+# # old: ##INFO=<ID=NS,Number=0,Type=Flag,Description="Non-synonymous SNP">
+# # new: ##INFO=<ID=NS,Number=1,Type=Integer,Description="Number of Samples With Data">
+
+# bcftools reheader \
+#     -h /scratch/ejy4bu/drosophila/inbred/combined_vcf/DRGP2.dm6.header.txt \
+#     -o /project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.vcf.gz \
+#     $vcf
 
 Rscript 1b.prepareGDS/vcf_to_gds.R
