ba09c0bf1a0b082e5bf8776f846388cd279dfc83
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-07-27 13:16:40 -0400

    rerun vcf to gds

--- 1b.prepareGDS/vcf_to_gds.R
+++ 1b.prepareGDS/vcf_to_gds.R
@@ -1,9 +1,9 @@
 library(SeqArray)
 
 vcf_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.vcf.gz"
-gds_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.gds"
-
-seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = TRUE)
+# gds_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.gds"
+gds_file <- "/scratch/ejy4bu/drosophila/gds_files/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.gds"
+seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = 8)
 
 #open and check file
 gds <- seqOpen(gds_file)
