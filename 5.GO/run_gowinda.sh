b87279a7c453d2b053a9c773269d0d80fe794090
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-07-27 22:58:40 -0400

    go 30

--- 5.GO/run_gowinda.sh
+++ 5.GO/run_gowinda.sh
@@ -17,8 +17,8 @@ BG_dir=/scratch/ejy4bu/drosophila/GO/gowinda/backgroundFiles/
 CANDIDATE_root=/scratch/ejy4bu/drosophila/GO/gowinda/candidateFiles/
 OUTPUT_root=/scratch/ejy4bu/drosophila/GO/gowinda/results
 
-MAF_dir=MAF25filter_polyAF
-CANDIDATE_id=25_polyAF
+MAF_dir=MAF30filter_polyAF
+CANDIDATE_id=30_polyAF
 
 #maf_inputs <- c(0.005, 0.01, 0.02, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.40, 0.49)
 # running maf-matching poly af 0.5, 1, 2, 5, 10
