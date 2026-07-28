3b7288cd3f2b05f454cb1a3ac6157c7fa981c1e7
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-07-28 14:44:29 -0400

    5

--- 5.GO/run_gowinda.sh
+++ 5.GO/run_gowinda.sh
@@ -17,8 +17,8 @@ BG_dir=/scratch/ejy4bu/drosophila/GO/gowinda/backgroundFiles/
 CANDIDATE_root=/scratch/ejy4bu/drosophila/GO/gowinda/candidateFiles/
 OUTPUT_root=/scratch/ejy4bu/drosophila/GO/gowinda/results
 
-MAF_dir=MAF2filter_globalAF
-CANDIDATE_id=2_globalAF
+MAF_dir=MAF5filter_globalAF
+CANDIDATE_id=5_globalAF
 
 #maf_inputs <- c(0.005, 0.01, 0.02, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.40, 0.49)
 # running noMAF global af 0.5
