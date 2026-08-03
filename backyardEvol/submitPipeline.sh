a1f214691da05fed8718b6f8c7e683a4ab67fc27
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-08-03 17:13:16 -0400

    sbatch

--- backyardEvol/submitPipeline.sh
+++ backyardEvol/submitPipeline.sh
@@ -2,14 +2,17 @@
 #
 #SBATCH -J make_bam_pipeline    # Job name
 #SBATCH --ntasks=1        # Single task per job
-#SBATCH --cpus-per-task=10 # Number of CPU cores per task
+#SBATCH --cpus-per-task=1 # Number of CPU cores per task
 #SBATCH -N 1              # Run on one node
 #SBATCH -t 0-18:00        # 10 hours runtime
-#SBATCH --mem=100G        # Memory per node
+#SBATCH --mem=5G        # Memory per node
 #SBATCH -o /scratch/ejy4bu/err_outs/be/pipeline.%A_%a.out # Standard output
 #SBATCH -e /scratch/ejy4bu/err_outs/be/pipeline.%A_%a.err # Standard error
 #SBATCH -p standard       # Partition
 #SBATCH --account=berglandlab
+
+set -euo pipefail
+
 
 SAMPLE_LIST="/scratch/ejy4bu/backyardEvolution/metadata/testSamples.txt"
 # SAMPLE_LIST="/scratch/ejy4bu/backyardEvolution/metadata/allSamples.txt"
