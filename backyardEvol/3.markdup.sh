d2edae6b56d74697df65b102c6826c6064d52aa8
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-08-03 15:29:27 -0400

    undo

--- backyardEvol/3.markdup.sh
+++ backyardEvol/3.markdup.sh
@@ -14,14 +14,14 @@
 set -euo pipefail
 
 ### to Run
-# # sbatch --array=1-$(wc -l < /scratch/ejy4bu/backyardEvolution/metadata/allSamples.txt)%20 ~/drosophila_polymorph/backyardEvol/1.removeAdapters.sh
-# SAMPLE_LIST="/scratch/ejy4bu/backyardEvolution/metadata/allSamples.txt"
-# sampName=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLE_LIST")
-# SAMPLE_DIR="/scratch/ejy4bu/backyardEvolution/fastq/${sampName}"
+# sbatch --array=1-$(wc -l < /scratch/ejy4bu/backyardEvolution/metadata/allSamples.txt)%20 ~/drosophila_polymorph/backyardEvol/1.removeAdapters.sh
+SAMPLE_LIST="/scratch/ejy4bu/backyardEvolution/metadata/allSamples.txt"
+sampName=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLE_LIST")
+SAMPLE_DIR="/scratch/ejy4bu/backyardEvolution/fastq/${sampName}"
 
-# to test: Dsimu_m_albe_2020_11_01_0092
-SAMPLE_DIR=/scratch/ejy4bu/backyardEvolution/fastq/Dsimu_m_albe_2020_11_01_0092/
-sampName=$(basename "$SAMPLE_DIR")
+# # to test: Dsimu_m_albe_2020_11_01_0092
+# SAMPLE_DIR=/scratch/ejy4bu/backyardEvolution/fastq/Dsimu_m_albe_2020_11_01_0092/
+# sampName=$(basename "$SAMPLE_DIR")
 
 module load picard
 
