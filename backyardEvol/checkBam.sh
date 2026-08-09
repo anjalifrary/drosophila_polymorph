0f2d0c37157396cbbc27c0d6e3f897de4ef8e13e
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-08-09 15:39:30 -0400

    smaller request

--- backyardEvol/checkBam.sh
+++ backyardEvol/checkBam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 #SBATCH -J be_checkbam # A single job name for the array
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH -N 1 # on one node
-#SBATCH -t 0-10:00 # 10 hours
+#SBATCH -t 0-01:00 # 10 hours
 #SBATCH --mem 20G
 #SBATCH -o /scratch/ejy4bu/err_outs/be/check/checkBAM.%A_%a.out # Standard output
 #SBATCH -e /scratch/ejy4bu/err_outs/be/check/checkBAM.%A_%a.err # Standard error
