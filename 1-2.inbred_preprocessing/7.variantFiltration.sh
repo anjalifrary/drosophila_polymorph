4734418fac4165b63f1818d7c29bb5f1977c008b
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-08-12 11:51:13 -0400

    varFilter sim

--- 1-2.inbred_preprocessing/7.variantFiltration.sh
+++ 1-2.inbred_preprocessing/7.variantFiltration.sh
@@ -16,17 +16,17 @@ module load bcftools
 module load gatk
 module load htslib
 
-### mel:
-ref=/project/berglandlab/anjali/drosophila_polymorphism/data_files/fastas/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.cleanNames.fna
-outdir="/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2/"
-in_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.norm.vcf.gz"
-out_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.norm.filtered.vcf.gz"
+# ### mel:
+# ref=/project/berglandlab/anjali/drosophila_polymorphism/data_files/fastas/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.cleanNames.fna
+# outdir="/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2/"
+# in_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.norm.vcf.gz"
+# out_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.norm.filtered.vcf.gz"
 
-# ### sim
-# ref="/project/berglandlab/anjali/drosophila_polymorphism/data_files/fastas/GCF_016746395.2_Prin_Dsim_3.1_genomic.cleanNames.fna"
-# outdir="/scratch/ejy4bu/drosophila/inbred/combined_vcf/dsim3.signor/"
-# in_vcf="${outdir}/dsim3.signor.combined.norm.vcf.gz"
-# out_vcf="${outdir}/dsim3.signor.combined.norm.filtered.vcf.gz"
+### sim
+ref="/project/berglandlab/anjali/drosophila_polymorphism/data_files/fastas/GCF_016746395.2_Prin_Dsim_3.1_genomic.cleanNames.fna"
+outdir="/scratch/ejy4bu/drosophila/inbred/combined_vcf/dsim3.signor/"
+in_vcf="${outdir}/dsim3.signor.combined.norm.vcf.gz"
+out_vcf="${outdir}/dsim3.signor.combined.norm.filtered.vcf.gz"
 
 ### 5. Filter variants (used GATK best practices hard filters.. ?)
 echo "Filtering..."
