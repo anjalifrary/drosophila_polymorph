dfdd78a803e29d05eeaa3ec793e78b1535d115ef
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-08-12 10:16:56 -0400

    filename

--- 1b.prepareGDS/vcf_to_gds.R
+++ 1b.prepareGDS/vcf_to_gds.R
@@ -1,8 +1,8 @@
 library(SeqArray)
 
-vcf_file <- "/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2.source_BCM-HGSC.dm6.final.norm.ann.eff.vcf.gz"
+vcf_file <- "/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2/DGRP2.source_BCM-HGSC.dm6.final.norm.ann.eff.vcf.gz"
 # gds_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.gds"
-gds_file <- "/scratch/ejy4bu/drosophila/gds_files/DGRP2.source_BCM-HGSC.dm6.final.norm.ann.eff.gds"
+gds_file <- "/scratch/ejy4bu/drosophila/inbred/gds_files/DGRP2.source_BCM-HGSC.dm6.final.norm.ann.eff.gds"
 
 seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = 8)
 
