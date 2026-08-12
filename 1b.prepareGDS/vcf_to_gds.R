d23c184e36f31fb8421fe22ec9acb1888b8f7765
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-08-12 10:15:36 -0400

    vcf to gds for mel

--- 1b.prepareGDS/vcf_to_gds.R
+++ 1b.prepareGDS/vcf_to_gds.R
@@ -1,8 +1,8 @@
 library(SeqArray)
 
-vcf_file <- "/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2.source_BCM-HGSC.dm6.final.norm.biallelic.snpsOnly.reheadered.ann.eff.vcf.gz"
+vcf_file <- "/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2.source_BCM-HGSC.dm6.final.norm.ann.eff.vcf.gz"
 # gds_file <- "/project/berglandlab/anjali/drosophila_polymorphism/data_files/vcfs/DGRP2.source_BCM-HGSC.dm6.final.reheadered.ann.eff.gds"
-gds_file <- "/scratch/ejy4bu/drosophila/gds_files/DGRP2.source_BCM-HGSC.dm6.final.norm.biallelic.snpsOnly.reheadered.ann.eff.gds"
+gds_file <- "/scratch/ejy4bu/drosophila/gds_files/DGRP2.source_BCM-HGSC.dm6.final.norm.ann.eff.gds"
 
 seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = 8)
 
@@ -10,3 +10,5 @@ seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = 8)
 gds <- seqOpen(gds_file)
 print(seqSummary(gds))
 seqClose(gds)
+
+file.copy(from = gds_file, to = "/project/berglandlab/anjali/drosophila_polymorphism/data_files/gds/")
