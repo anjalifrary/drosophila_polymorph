8e102f8224fa29bd684f1b0549573bb73b10432c
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-09-07 12:47:44 -0400

    .

---
+++ 1-3.poolseq_preprocessing/3.indelfilt.sh
@@ -0,0 +1,16 @@
+
+
+
+# ### DEST mel:
+# outdir="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/filtering/"
+# in_vcf="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/dest.PoolSeq.SNAPE.001.50.03Dec2024_DACtest.norep.ann.eff.vcf.gz"
+# gap_vcf="${outdir}/dest.PoolSeq.SNAPE.001.50.03Dec2024_DACtest.norep.ann.eff.snpgap10.vcf.gz"
+# snp_vcf="${outdir}/dest.PoolSeq.SNAPE.001.50.03Dec2024_DACtest.norep.ann.eff.snpgap10.snpsOnly.vcf.gz"
+
+### DEST sim:
+outdir="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/filtering/"
+in_vcf="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/dest.sim.all.SNAPE.001.50.20Nov2025_sim.norep.NOREP.ann.eff.vcf.gz"
+gap_vcf="${outdir}/dest.sim.all.SNAPE.001.50.20Nov2025_sim.norep.NOREP.ann.eff.snpgap10.vcf.gz"
+snp_vcf="${outdir}/dest.sim.all.SNAPE.001.50.20Nov2025_sim.norep.NOREP.ann.eff.snpgap10.snpsOnly.vcf.gz"
+
+
