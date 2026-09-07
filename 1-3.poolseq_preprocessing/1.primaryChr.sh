f641ea2740158fecbae51fde35d22aefff11b4f7
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-09-07 12:54:21 -0400

    .

--- 1-3.poolseq_preprocessing/1.primaryChr.sh
+++ 1-3.poolseq_preprocessing/1.primaryChr.sh
@@ -19,7 +19,7 @@
 # snp_vcf="${outdir}/dest.PoolSeq.SNAPE.001.50.03Dec2024_DACtest.norep.ann.eff.snpgap10.snpsOnly.vcf.gz"
 
 # ### DEST sim:
-# outdir="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/filtering/"
+outdir="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/filtering/"
 in_vcf="/scratch/ejy4bu/drosophila/DEST_remake/vcfs/dest.sim.all.SNAPE.001.50.20Nov2025_sim.norep.NOREP.ann.eff.vcf.gz"
 # gap_vcf="${outdir}/dest.sim.all.SNAPE.001.50.20Nov2025_sim.norep.NOREP.ann.eff.snpgap10.vcf.gz"
 # snp_vcf="${outdir}/dest.sim.all.SNAPE.001.50.20Nov2025_sim.norep.NOREP.ann.eff.snpgap10.snpsOnly.vcf.gz"
@@ -38,6 +38,8 @@ bcftools annotate \
     -Oz \
     -o $out_vcf \
     $in_vcf
+
+echo "Renamed chrs"
 
 bcftools query -f '%CHROM\n' "$out_vcf" | sort -u
 
