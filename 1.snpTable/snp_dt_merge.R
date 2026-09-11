6183bd15b3cce38fb5d45064f2f33e70df214896
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-09-11 12:08:39 -0400

    working on id'ing codons

--- 1.snpTable/snp_dt_merge.R
+++ 1.snpTable/snp_dt_merge.R
@@ -41,6 +41,67 @@ out_rds <- paste0(out_dir, "dsim3.signor.DGRP2.source_BCM-HGSC.all_quality_varia
 shared_table <- merge(mel_snp_dt, sim_snp_dt, by = c("chr", "pos"), suffixes = c("_mel", "_sim"), all=T)
 message(nrow(shared_table), " total variants")
 
+
+library(AnnotationDbi)
+library(org.Dm.eg.db)
+gene_map <- AnnotationDbi::select(
+    org.Dm.eg.db,
+    keys = unique(shared_table$gene_mel),
+    keytype = "SYMBOL",
+    columns = c("SYMBOL", "FLYBASE")
+)
+
+gene_map <- as.data.table(gene_map)
+setnames(gene_map, c("SYMBOL", "FLYBASE"), c("gene_mel", "gene_id_fbgn"))
+
+gene_map <- unique(gene_map[, .(gene_mel, gene_id_fbgn)])
+
+shared_table[gene_map, gene_id_fbgn := i.gene_id_fbgn, on = "gene_mel"]
+
+shared_table[, aa_ref_mel := gsub("^p\\.([[:alpha:]]{3}).*", "\\1" , aa_change_mel)]   # Ser
+shared_table[, aa_alt_mel := gsub("^p\\.[A-Za-z]{3}[0-9]+([A-Za-z]{3})/c\\..*","\\1", aa_change_mel)]
+
+shared_table[, aa_ref_sim := gsub("^p\\.([[:alpha:]]{3}).*", "\\1" , aa_change_sim)]   
+shared_table[, aa_alt_sim := gsub("^p\\.[A-Za-z]{3}[0-9]+([A-Za-z]{3})/c\\..*","\\1", aa_change_sim)]
+
+shared_table[, aa_pos_mel := gsub(".*?([0-9]+).*", "\\1", aa_change_mel)]
+shared_table[, aa_pos_sim := gsub(".*?([0-9]+).*", "\\1", aa_change_sim)]
+
+shared_table[, codon_ref_mel := gsub("^([[:alpha:]]{3})/.*", "\\1", codon_change_mel)] # aGc
+shared_table[, codon_alt_mel := gsub(".*/([[:alpha:]]{3})$", "\\1", codon_change_mel)] # aTc
+
+shared_table[, codon_ref_sim := gsub("^([[:alpha:]]{3})/.*", "\\1", codon_change_sim)] # aGc
+shared_table[, codon_alt_sim := gsub(".*/([[:alpha:]]{3})$", "\\1", codon_change_sim)] # aTc
+
+
+
+mel_dt <- shared_table[!is.na(ref_mel), .(
+        chr, pos, 
+        ref_mel, alt_mel, 
+        codon_ref_mel, codon_alt_mel, 
+        gene_id_fbgn, gene_mel
+    )
+]
+
+sim_dt <- shared_table[!is.na(ref_sim), .(
+        chr, pos, 
+        ref_sim, alt_sim, 
+        codon_ref_sim, codon_alt_sim,
+        gene_sim
+    )
+]
+
+setorder(mel_dt, chr, pos)
+mel_dt[, dist_prev_mel := pos - data.table::shift(pos), by = chr]
+mel_dt[, dist_next_mel := data.table::shift(pos, type = "lead") - pos, by = chr]
+
+setorder(sim_dt, chr, pos)
+sim_dt[, dist_prev_sim := pos - data.table::shift(pos), by = chr]
+sim_dt[, dist_next_sim := data.table::shift(pos, type = "lead") - pos, by = chr]
+
+shared_table <- merge(shared_table, mel_dt[, .(chr, pos, dist_prev_mel, dist_next_mel)], by=c("chr", "pos"))
+shared_table <- merge(shared_table, sim_dt[, .(chr, pos, dist_prev_sim, dist_next_sim)], by=c("chr", "pos"))
+
 message("saving rds to ", out_rds)
 saveRDS(shared_table, out_rds)
 
--- 2.classification/voi_preClassification_9-9-26.R
+++ 2.classification/voi_preClassification_9-9-26.R
@@ -15,12 +15,51 @@ in_rds <- paste0(out_dir, "dsim3.signor.DGRP2.source_BCM-HGSC.all_quality_varian
 background_rds <- paste0(out_dir, "dsim3.signor.DGRP2.source_BCM-HGSC.background_1polymorphicCodon.rds")
 candidate_rds <- paste0(out_dir, "dsim3.signor.DGRP2.source_BCM-HGSC.candidate_sharedPolyCodonsOnly.rds")
 
-unfiltered_dt <- readRDS(in_rds)
+filtered_dt <- readRDS(in_rds)
 
-set.seed(123)
+# testing on mel only , chr 2L
+mel_dt_2L <- filtered_dt[
+    !is.na(ref_mel) & chr=="2L", .(
+        chr, pos, classification, 
+        ref_mel, alt_mel, 
+        codon_ref_mel, codon_alt_mel, 
+        gene_id_fbgn, gene_mel
+    )
+]
 
-test_dt <- unfiltered_dt[sample(.N, 1000)]  
+sim_dt_2L <- filtered_dt[
+    !is.na(ref_sim) & chr=="2L", .(
+        chr, pos, classification, 
+        ref_sim, alt_sim, 
+        codon_ref_sim, codon_alt_sim,
+        gene_sim
+    )
+]
+unfiltered_dt <- readRDS(paste0(out_dir, "dsim3.signor.DGRP2.source_BCM-HGSC.all_quality_variants_merge_unfilt.rds"))
 
-# nrow(test_dt[!is.na(variant.id_mel)])
-# nrow(test_dt[!is.na(variant.id_sim)])
-# nrow(test_dt[!is.na(variant.id_mel) & !is.na(variant.id_sim)])
+unfiltered_dt[, aa_pos_mel := gsub(".*?([0-9]+).*", "\\1", aa_change_mel)]
+unfiltered_dt[, aa_pos_sim := gsub(".*?([0-9]+).*", "\\1", aa_change_sim)]
+
+mel_dt_2L <- merge(mel_dt_2L, unfiltered_dt[, .(chr, pos, transcript_id_mel, aa_pos_mel)], by=c("chr", "pos"))
+same_aa_mel <- mel_dt_2L[
+    , if (.N>1) .SD,
+    by=.(chr, aa_pos_mel, transcript_id_mel)
+]
+setorder(mel_dt_2L, chr, pos)
+mel_dt_2L[, dist_prev := pos - shift(pos), by = chr]
+mel_dt_2L[, dist_next := shift(pos, type = "lead") - pos, by = chr]
+
+
+
+
+sim_dt_2L <- merge(sim_dt_2L, unfiltered_dt[, .(chr, pos, transcript_id_sim, aa_pos_sim)], by=c("chr", "pos"))
+
+same_aa_sim <- sim_dt_2L[
+    , if (.N>1) .SD,
+    by=.(chr, aa_pos_sim, transcript_id_sim)
+]
+
+setorder(sim_dt_2L, chr, pos)
+sim_dt_2L[, dist_prev := pos - shift(pos), by = chr]
+sim_dt_2L[, dist_next := shift(pos, type = "lead") - pos, by = chr]
+
