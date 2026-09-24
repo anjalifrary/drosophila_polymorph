8d855dba991df5b4ef141d84209dce3d8ab5a68c
anjalifrary <167632860+anjalifrary@users.noreply.github.com>
2026-09-24 18:39:53 -0400

    trying repeat mask...

--- 1-2.inbred_preprocessing/8.snpEff_wCodon.sh
+++ 1-2.inbred_preprocessing/8.snpEff_wCodon.sh
@@ -20,7 +20,8 @@ SNPEFF=/project/berglandlab/multispecies_endemism/snpEFF/v4.3t/snpEff/
 
 ### mel:
 outdir="/scratch/ejy4bu/drosophila/inbred/combined_vcf/DGRP2/"
-in_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.reheadered.primaryChr.norm.gatkfilt.snpgap10.snpsOnly.repeatmasked.wmdust.vcf.gz"
+
+in_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.reheadered.primaryChr.norm.gatkfilt.snpgap10.snpsOnly.vcf.gz"
 out_vcf="${outdir}/DGRP2.source_BCM-HGSC.dm6.final.reheadered.primaryChr.norm.gatkfilt.snpgap10.snpsOnly.repeatmasked.wmdust.ann.eff.vcf.gz"
 
 echo "Annotating Dm6 vcf with SnpEff..."
